25f038f42e5a1807f2b7f1a9b838c94cc472e1f0
DMahyuddin <mahyuddin.22dylan@gmail.com>
2024-03-22 20:46:04 -0400

    added vitePreprocess in Svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: vitePreprocess(),
+
   kit: {
     adapter: adapter({
       fallback: "404.html",
